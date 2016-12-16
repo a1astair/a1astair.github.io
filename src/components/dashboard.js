5bdfe97b85c382679a05a91173f0015737cc6699
a1astair <a.beaumont11@gmail.com>
2016-12-15 21:51:47 -0800

    Fix spacing errors

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -12,29 +12,33 @@ export default class Dashboard extends Component {
 
   render() {
     return (
-        <div className="off-canvas-wrapper">
+      <div className="off-canvas-wrapper">
         <div className="off-canvas-wrapper-inner" data-off-canvas-wrapper="">
-        <div className="off-canvas position-left reveal-for-large" id="my-info" data-off-canvas="" data-position="left" aria-hidden="true" data-offcanvas="y7t9hf-offcanvas">
-        <div className="row column">
-        <br/>
-        <img className="thumbnail" src={profilePic}/>
-        <h5 className="text-center">Contact Info</h5>
-        <p><i className="fa fa-envelope" aria-hidden="true"></i> a.beaumont11@gmail.com</p>
-        <p><i className="fa fa-phone" aria-hidden="true"></i> 778-350-0061</p>
-        </div>
-        </div>
-        <div className="off-canvas-content" data-off-canvas-content="">
-        <div className="title-bar hide-for-large">
-        <div className="title-bar-left">
-        <button className="menu-icon" type="button" data-open="my-info" aria-expanded="false" aria-controls="my-info"></button>
-        <span className="title-bar-title"></span>
-        </div>
-        </div>
-        <div className="callout primary">
-        <div className="row column">
-        <h1>Alastair Beaumont</h1>
-        </div>
-        </div>
+          <div className="off-canvas position-left reveal-for-large" id="my-info" data-off-canvas="" data-position="left" aria-hidden="true" data-offcanvas="y7t9hf-offcanvas">
+            <div className="row column">
+              <br/>
+              <img className="thumbnail" src={profilePic}/>
+              <h5 className="text-center">Contact Info</h5>
+              <p>
+                <i className="fa fa-envelope" aria-hidden="true"></i> a.beaumont11@gmail.com
+              </p>
+              <p>
+                <i className="fa fa-phone" aria-hidden="true"></i> 778-350-0061
+              </p>
+            </div>
+          </div>
+          <div className="off-canvas-content" data-off-canvas-content="">
+            <div className="title-bar hide-for-large">
+              <div className="title-bar-left">
+                <button className="menu-icon" type="button" data-open="my-info" aria-expanded="false" aria-controls="my-info"></button>
+                <span className="title-bar-title"></span>
+              </div>
+            </div>
+            <div className="callout primary">
+              <div className="row column">
+                <h1>Alastair Beaumont</h1>
+              </div>
+            </div>
         <hr/>
         <div className="row">
           <div className="small-12 medium-8 columns medium-offset-2 text-center">
@@ -45,20 +49,20 @@ export default class Dashboard extends Component {
         <br/>
         <div className="row">
           <div className="small-3 columns text-center">
-            <a href="http://www.github.com/a1astair">
+            <a href="http://www.github.com/a1astair" target='_blank'>
               <i className="fa fa-github fa-5x" aria-hidden="true"></i>
               <p>Github</p>
             </a>
           </div>
           <div className="small-3 columns text-center">
-            <a href="https://www.linkedin.com/in/alastair-beaumont-a37a29b0">
+            <a href="https://www.linkedin.com/in/alastair-beaumont-a37a29b0" target='_blank'>
               <i className="fa fa-linkedin fa-5x" aria-hidden="true"></i>
               <p>LinkedIn</p>
             </a>
           </div>
           <br/>
           <div className="small-3 columns text-center">
-            <a href="./AlastairBeaumontResume.pdf">
+            <a href="./AlastairBeaumontResume.pdf" target='_blank'>
               <i className="fa fa-file fa-5x" aria-hidden="true"></i>
               <p>Resume</p>
             </a>
@@ -67,7 +71,7 @@ export default class Dashboard extends Component {
           <div className="small-3 columns text-center">
             <Link to={`/streamFinder`}>
               <i className="fa fa-futbol-o fa-5x" aria-hidden="true"></i>
-              <p>Steam Finder</p>
+              <p>Stream Finder</p>
             </Link>
           </div>
         </div>
